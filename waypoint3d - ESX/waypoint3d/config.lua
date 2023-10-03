223c23931074ff03a448eff5433a45e6a6ce946b
Xpance <67835224+XpanceWhy@users.noreply.github.com>
2023-10-03 17:12:47 +0300

    Add files via upload

---
+++ waypoint3d - ESX/waypoint3d/config.lua
@@ -0,0 +1,11 @@
+marker = {
+    id = 1,
+    scale = 10.0,
+    rgb = {r = 0, g = 255, b = 0, a = 255},
+    height = 30.0
+}
+
+textureDict = 'marker'
+textureName = 'marker'
+textureMarkerScale = 15.0
+textureHeight = 34.0
---
+++ waypoint3d - ESX/waypoint3d/fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+
+game 'gta5'
+author 'Vestige'
+description '3D Waypoint Script'
+
+version '1.0'
+
+client_scripts {
+	'config.lua',
+	'main.lua',
+}
